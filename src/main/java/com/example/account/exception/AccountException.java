1b78e0eb127e5ab2f6d22dbaaad503819a63e8b4
tkdlqh2 <yhj7124@naver.com>
2022-07-08 18:14:36 +0900

    create exception package & account exception

---
+++ src/main/java/com/example/account/exception/AccountException.java
@@ -0,0 +1,13 @@
+package com.example.account.exception;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AccountException extends RuntimeException{
+    private ErrorCode errorCode;
+    private String errorMessage;
+}
