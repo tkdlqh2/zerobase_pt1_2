cdc0c763011bcb64e45359946ea460be2e566499
tkdlqh2 <yhj7124@naver.com>
2022-07-10 01:21:22 +0900

    make Transaction method

--- src/main/java/com/example/account/type/ErrorCode.java
+++ src/main/java/com/example/account/type/ErrorCode.java
@@ -11,6 +11,7 @@ public enum ErrorCode {
     USER_ACCOUNT_UN_MATCH("사용자와 계좌가 일치하지 않습니다."),
     ACCOUNT_ALREADY_UNREGISTERED("이미 해지된 계좌입니다."),
     BALANCE_NOT_EMPTY("잔액이 있는 계좌는 해질할 수 없습니다."),
+    AMOUNT_EXCEED_BALANCE("잔액이 부족합니다."),
     MAX_ACCOUNT_PER_USER_10("사용자의 최대 계좌 개수는 10개입니다.");
 
     private final String description;
